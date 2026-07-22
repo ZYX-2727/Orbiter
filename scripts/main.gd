8104b6e6514dd7d6dce727f14b3877dc9ff04afc
ZYX-2727 <pauld.skibidi@gmail.com>
2026-07-22 14:39:45 -0500

    add highscore

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,15 +5,34 @@ var between_asteroids = 1.5
 var asteroid_pre: PackedScene
 var player_pre: PackedScene
 var playing: bool = false
+var first_time: bool
 
 var score: int = 0
+var high_score: int
 #const SCORE_MULT: float = 1000
 var asteroids_destroyed: int = 0
 var distance: float #In km
 const SPEED: float = 7.66 #km/s
 
+func save_data(section: String, key: String, value) -> void:
+	var data = ConfigFile.new()
+	data.set_value(section, key, value)
+	data.save("user://data.cfg")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var data = ConfigFile.new()
+	var error = data.load("user://data.cfg")
+	
+	if error != OK:
+		#First time playing
+		first_time = true
+	
+	print(first_time)
+	
+	high_score = data.get_value("player", "high_score", 0)
+	$Menu/Panel/High.text = "High Score: " + str(high_score)
+	
 	asteroid_pre = preload("res://scenes/asteroid.tscn")
 	player_pre = preload("res://scenes/player.tscn")
 	EventBus.connect("death", _on_death)
@@ -45,6 +64,15 @@ func _on_death() -> void:
 	$HUD.hide()
 	$Menu.show()
 	$Menu/Panel/Score.text = "Score: " + str(score)
+	$Menu/Panel/High.text = "High Score: " + str(high_score)
+	
+	if score > high_score:
+		high_score = score
+		save_data("player", "high_score", high_score)
+		$Menu/Panel/High.text = "New High!"
+		$Menu/Panel/High.add_theme_color_override("font_color", Color(255, 255, 0))
+	else:
+		$Menu/Panel/High.add_theme_color_override("font_color", Color(255, 255, 255))
 	
 	#Cleanup vars
 	score = 0
